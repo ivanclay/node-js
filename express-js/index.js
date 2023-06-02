15d26513d6d7cc2c84938d422e3a74b3b2ed74d9
Ivan Clay <ivan.clay.moura@gmail.com>
2023-06-02 08:46:55 -0300

    feat: methods

--- express-js/index.js
+++ express-js/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
 
 const app = express();
 
@@ -9,6 +10,18 @@ app.get("/", (req,res)=> {
     // res.set("Content-Type", "text/html");
     res.type("html");
     res.send("<h1>Hello World from get</h1>");
+});
+
+app.post("/", (req,res)=> {
+    res.send("<h1>Hello World from POST</h1>");
+});
+
+app.put("/", (req,res)=> {
+    res.send("<h1>Hello World from PUT</h1>");
+});
+
+app.delete("/", (req,res)=> {
+    res.send("<h1>Hello World from DELETE</h1>");
 });
 
 const PORT = 5000;
