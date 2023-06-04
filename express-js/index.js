385f4cfd57642b5281e2f2e805e93d33e888f01e
Ivan Clay <ivan.clay.moura@gmail.com>
2023-06-04 19:05:02 -0300

    feat: routes

--- express-js/index.js
+++ express-js/index.js
@@ -1,28 +1,32 @@
 const express = require('express');
-const path = require('path');
-const bodyParser = require('body-parser');
-
 const app = express();
+var aluno = require('./routes/aluno');
 
-app.use(express.static(path.join(__dirname, 'client')));
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded()); //form
+
+// const path = require('path');
+// app.use(express.static(path.join(__dirname, 'client')));
+
+app.use('/api', aluno);
 
 app.get("/", (req,res)=> {
     // res.set("Content-Type", "text/html");
-    res.type("html");
+    // res.type("html");
     res.send("<h1>Hello World from get</h1>");
 });
 
-app.post("/", (req,res)=> {
-    res.send("<h1>Hello World from POST</h1>");
-});
+// app.post("/", (req,res)=> {
+//     res.send("<h1>Hello World from POST</h1>");
+// });
 
-app.put("/", (req,res)=> {
-    res.send("<h1>Hello World from PUT</h1>");
-});
+// app.put("/", (req,res)=> {
+//     res.send("<h1>Hello World from PUT</h1>");
+// });
 
-app.delete("/", (req,res)=> {
-    res.send("<h1>Hello World from DELETE</h1>");
-});
+// app.delete("/", (req,res)=> {
+//     res.send("<h1>Hello World from DELETE</h1>");
+// });
 
 const PORT = 5000;
 
